4c676c1cf86a7a10a46daf90ef069b77a7b25339
AdityaAgg <adityaaggarwalz200@gmail.com>
2023-02-21 20:22:38 -0800

    Adding unit tests

--- services/functions/billing.js
+++ services/functions/billing.js
@@ -3,13 +3,14 @@ import handler from "../util/handler";
 import { calculateCost } from "../util/cost";
 
 export const main = handler(async (event) => {
+    console.log("xerus");
     const { storage, source } = JSON.parse(event.body);
     const amount = calculateCost(storage);
     const description = "Scratch charge";
-
+    console.log("apple");
     // Load our secret key from the  environment variables
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
+    console.log("bannana");
     await stripe.charges.create({
         source,
         amount,
